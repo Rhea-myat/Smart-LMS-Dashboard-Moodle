8b0d47dbf8eb9a295907dfdfa49d863f6d5ae276
ayeio <105025604+Rhea-myat@users.noreply.github.com>
2026-06-16 20:20:23 +0800

    updated env

--- etl/db.py
+++ etl/db.py
@@ -2,5 +2,6 @@ from sqlalchemy import create_engine
 
 def get_engine():
     return create_engine(
-        "mysql+pymysql://root:root@localhost:8889/analytics_db"
+        #"mysql+pymysql://root:root@localhost:8889/analytics_db"
+        "mysql+pymysql://ft04user:Asdfg09876@127.0.0.1:3306/analytics_db"
     )
