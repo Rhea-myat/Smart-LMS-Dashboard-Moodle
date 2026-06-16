746486beb9ca4992f3d30e2bf9ac365cc6c65904
Yi Mon <130958801+LinLaiYiMon@users.noreply.github.com>
2026-06-16 15:24:01 +0800

    Update version.php

--- studentengagement/version.php
+++ studentengagement/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_studentengagement';
-$plugin->version   = 2026060200;
-$plugin->requires  = 2026042000; // Moodle 5.x baseline 
+$plugin->version   = 2026061600;
+$plugin->requires  = 2022112800; // Moodle 4.x baseline 
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = 'v1.0';
