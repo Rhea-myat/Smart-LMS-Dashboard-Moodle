c57d8c9e33bfb30b9a1d5ed92656fe37d80cb3da
ayeio <105025604+Rhea-myat@users.noreply.github.com>
2026-06-16 20:51:47 +0800

    remove .env file

--- etl/db.py
+++ etl/db.py
@@ -1,7 +1,25 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+"""
 def get_engine():
     return create_engine(
         #"mysql+pymysql://root:root@localhost:8889/analytics_db"
         "mysql+pymysql://ft04user:Asdfg09876@127.0.0.1:3306/analytics_db"
+    )
+"""
+
+
+def get_engine():
+    return create_engine(
+        f"mysql+pymysql://"
+        f"{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+
     )
